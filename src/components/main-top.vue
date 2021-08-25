399743a7e191c9d1277d4471336a608589b010a4
chisNa <sosoxiaona@qq.com>
2021-08-25 14:54:23 +0800

    头部样式调整

--- src/components/main-top.vue
+++ src/components/main-top.vue
@@ -1,45 +1,39 @@
 <template>
   <div>
-    <table
-      style="width:100%;height:80px;background:#4F83E9 url('static/image/top_bg.png');background-position:center center;background-repeat:no-repeat; border-spacing: 0px;"
-      id="navTable"
-    >
-      <tr>
-        <td style="width:420px;text-align:center;-webkit-app-region:drag;-webkit-user-select:none;">
-          <img src="@/components/assets/image/coal-logo.png" />
-        </td>
-        <td style="width:30px;-webkit-app-region:drag;-webkit-user-select:none;">&nbsp;</td>
-        <td
+    <div class="main-top-main">
+      <div style="margin-left: 10px;">
+        <!-- 系统名称图 -->
+        <img src="@/components/assets/image/coal-logo.png" />
+      </div>
+      <div class="main-top-nav">
+        <!-- 页签 -->
+        <div
           class="navTd"
           :style="activeTab === 'SourceDownload' ? 'background: #224f7d;' : ''"
           @click="changeTab('SourceDownload')">
-          <!-- <a class="topNav title" href="javascript:navDownload()">资源下载</a> -->
-          <a class="topNav title">资源下载</a>
-        </td>
-        <td
+          <span>资源下载</span>
+        </div>
+        <div
           class="navTd"
           :style="activeTab === 'MakeLawWrit' ? 'background: #224f7d;' : ''"
           @click="changeTab('MakeLawWrit')">
-          <!-- <a class="topNav title" href="javascript:navWork()">执法工作台</a> -->
-          <a class="topNav title">执法工作台</a>
-        </td>
-        <!--<td style="width:100px;text-align:center;"><a class="topNav" href="javascript:navSurvey()">调查互动</a></td>-->
-        <td
+          <span>执法工作台</span>
+        </div>
+        <div
           class="navTd"
           :style="activeTab === 'WritManagement' ? 'background: #224f7d;' : ''"
           @click="changeTab('WritManagement')">
-          <!-- <a class="topNav title" href="javascript:navDoc()">文书管理</a> -->
-          <a class="topNav title">文书管理</a>
-        </td>
-        <td style="width:auto;-webkit-app-region:drag;-webkit-user-select:none;"></td>
-        <td style="width:150px;text-align:right;-webkit-app-region:no-drag;">
-          <el-button type="text" @click="logoutHandle">退出</el-button>
-          <img src="@/components/assets/image/min.png" id="minbt" @click="handleWindow('minWindow')" />&nbsp;
-          <img src="@/components/assets/image/maxed.png" id="maxbt" @click="handleWindow('maxWindow')" />&nbsp;
-          <img src="@/components/assets/image/close.png" id="closebt" @click="handleWindow('closeWindow')" />&nbsp;&nbsp;
-        </td>
-      </tr>
-    </table>
+          <span>文书管理</span>
+        </div>
+      </div>
+      <div class="main-top-operation">
+        <!-- 操作 -->
+        <el-button type="text" @click="logoutHandle">退出</el-button>
+        <img src="@/components/assets/image/min.png" id="minbt" @click="handleWindow('minWindow')" />&nbsp;
+        <img src="@/components/assets/image/maxed.png" id="maxbt" @click="handleWindow('maxWindow')" />&nbsp;
+        <img src="@/components/assets/image/close.png" id="closebt" @click="handleWindow('closeWindow')" />&nbsp;&nbsp;
+      </div>
+    </div>
   </div>
 </template>
 
@@ -109,13 +103,38 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.navTd {
-  width:100px;
-  text-align:center;
-  &:hover {
-    background: rgba(#224f7d, 0.6);
+.main-top-main {
+  width: 100%;
+  height: 80px;
+  background: url('~@/assets/img/top_bg.png');
+  background-position: center center;
+  border-spacing: 0px;
+  padding: 0 20px;
+  display: flex;
+  .main-top-nav {
+    margin-left: 40px;
+    display: flex;
+    .navTd {
+      width: 100px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: #fff;
+      font-size: 18px;
+      font-weight: 500;
+      &:hover {
+        background: rgba(#224f7d, 0.6);
+      }
+    }
+  }
+  .main-top-operation {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
   }
 }
+
 .topNav {
   color: #f7f7f7;
   text-decoration: none;
