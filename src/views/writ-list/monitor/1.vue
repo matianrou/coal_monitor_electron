46c049eba93d43a8fa2fda444a002611ea887302
matianrou <m18830290715@163.com>
2021-10-28 17:51:13 +0800

    增加两个文书页面加强和改善安全监管建议书  加强和改善安全管理意见书议书

---
+++ src/views/writ-list/monitor/1.vue
@@ -0,0 +1,343 @@
+<!-- 意见建议书 加强和改善安全监管建议书  -->
+<template>
+  <div style="width: 100%; height: 100%">
+    <let-main
+      ref="letMain"
+      :corp-data="corpData"
+      :doc-data="docData"
+      :let-data="letData"
+      :edit-data="editData"
+      @go-back="goBack"
+    >
+      <div slot="left">
+        <div class="page page-sizeA4">
+          <div>
+            <div class="stdRowH"></div>
+            <div class="textAlignCenter formHeader0">
+              国 家 矿 山 安 全 监 察
+              <br />
+            </div>
+            <div class="textAlignCenter formHeader3">
+              加强和改善安全监管建议书
+            </div>
+            <div class="docTextLine paper-number-div">
+              <div>
+                <span @click="commandFill('cellIdx0', '', 'TextItem')">{{
+                  letData.cellIdx0 ? letData.cellIdx0 : "（编辑）"
+                }}</span>
+                <label>矿安监</label>
+                <span @click="commandFill('cellIdx1', '', 'TextItem')">{{
+                  letData.cellIdx1 ? letData.cellIdx1 : "（编辑）"
+                }}</span>
+                <label>监建〔</label>
+                <span @click="commandFill('cellIdx2', '', 'TextItem')">{{
+                  letData.cellIdx2 ? letData.cellIdx2 : "（编辑）"
+                }}</span>
+                <label>〕</label>
+                <span @click="commandFill('cellIdx3', '', 'TextItem')">{{
+                  letData.cellIdx3 ? letData.cellIdx3 : "（编辑）"
+                }}</span>
+                <label>号</label>
+              </div>
+            </div>
+            <div class="docTextLine paper-number-div">
+              <div>
+                <label>签发人：</label>
+                <span
+                  class="line"
+                  @click="commandFill('cellIdx4', '签发人', 'TextItem')"
+                  >{{
+                    letData.cellIdx4 ? letData.cellIdx4 : "（点击编辑）"
+                  }}</span
+                >
+              </div>
+            </div>
+            <table class="docBody">
+              <tr>
+                <td
+                  class="cellInput cellBottomLine"
+                  id="cell_idx_5"
+                  style="width: 62%"
+                  data-title
+                  data-type="text"
+                  data-src
+                  @click="commandFill('cellIdx5', '', 'TextItem')"
+                >
+                  {{ letData.cellIdx5 }}
+                </td>
+                <td class="textAlignLeft">:</td>
+              </tr>
+            </table>
+            <div class="docTextarea">
+              <label style="width: 5%"></label>
+              经我
+              <span class="no-underline" @click="commandFill('cellIdx6', '局', 'TextItem')">{{
+                letData.cellIdx6 ? letData.cellIdx6 : ""
+              }}</span>
+              检查，
+              <span @click="commandFill('cellIdx7', '市', 'TextItem')">{{
+                letData.cellIdx7 ? letData.cellIdx7 : "（点击编辑）"
+              }}</span>
+              在矿山安全监督管理工作中，存在下列问题（见附件），现提出加强和改善矿山安全监管的如下建议（见附件）。
+            </div>
+            <div class="docTextarea">
+              <label style="width: 5%"></label>
+             请将处理意见于
+              <span @click="commandFill('cellIdx8', '年', 'TextItem')">{{
+                letData.cellIdx8 ? letData.cellIdx8 : "（XX）"
+              }}</span>
+              年
+              <span @click="commandFill('cellIdx9', '月', 'TextItem')">{{
+                letData.cellIdx9 ? letData.cellIdx9 : "（XX）"
+              }}</span>
+              月
+              <span @click="commandFill('cellIdx10', '日', 'TextItem')">{{
+                letData.cellIdx10 ? letData.cellIdx10 : "（XX）"
+              }}</span>
+              日前函告我
+              <span @click="commandFill('cellIdx10', '局', 'TextItem')">{{
+                letData.cellIdx10 ? letData.cellIdx10 : ""
+              }}</span>
+            </div>
+            <div class="docTextLine">
+              <label style="width: 5%"></label>
+              <label>附件：</label>
+              <div
+                class="no-line-div"
+                @click="
+                  commandFill(
+                    'cellIdx19',
+                    '加强和改善安全监管建议',
+                    'CheckTableItem'
+                  )
+                "
+              >
+                加强和改善安全监管建议
+              </div>
+            </div>
+            <table height="60"></table>
+            <div class="docTextLine">
+              <label>送件人（签名)：</label>
+              <div
+                class="line-div"
+                @click="
+                  commandFill('cellIdx12', '送件人（签名)', 'TextItem')
+                "
+              >
+                {{ letData.cellIdx12 ? letData.cellIdx12 : "（编辑）" }}
+              </div>
+              <label>日期：</label>
+              <div
+                class="line-div"
+                @click="commandFill('cellIdx13', '日期:', 'DateItem')"
+              >
+                {{ letData.cellIdx13 ? letData.cellIdx13 : "（编辑）" }}
+              </div>
+            </div>
+            <div class="docTextLine">
+              <label>收件人（签名)：</label>
+              <div
+                class="line-div"
+                @click="commandFill('cellIdx14', '收件人（签名)', 'TextItem')"
+              >
+                {{ letData.cellIdx14 ? letData.cellIdx14 : "（编辑）" }}
+              </div>
+              <label>日期：</label>
+              <div
+                class="line-div"
+                @click="commandFill('cellIdx15', '日期:', 'DateItem')"
+              >
+                {{ letData.cellIdx15 ? letData.cellIdx15 : "（编辑）" }}
+              </div>
+            </div>
+            <div class="docTextarea">
+              <span class="no-line"
+                >&nbsp;&nbsp;&nbsp;&nbsp;报 送：</span
+              >
+              <span
+                @click="commandFill('cellIdx16', '报送', 'TextItem')"
+                >{{
+                  letData.cellIdx16 ? letData.cellIdx16 : "（点击编辑）"
+                }}</span
+              >
+              <div class="line"></div>
+            </div>
+            <table height="30"></table>
+            <table class="docBody">
+              <tr>
+                <td
+                  class="cellInput"
+                  id="cell_idx_17"
+                  align="right"
+                  style="width: 95%"
+                  @click="commandFill('cellIdx17', '', 'TextItem')"
+                >
+                  {{ letData.cellIdx17 ? letData.cellIdx17 : "（点击编辑）" }}
+                </td>
+              </tr>
+                <tr>
+                <td
+                  class="cellInput"
+                  id="cell_idx_18"
+                  align="right"
+                  style="width: 95%"
+                  data-title
+                  data-type="date"
+                  data-src
+                  @click="commandFill('cellIdx18', '日期', 'DateItem')"
+                >
+                  {{ letData.cellIdx18 ? letData.cellIdx18 : "（点击编辑）" }}
+                </td>
+              </tr>
+            </table>
+            <table class="cellLine">
+              <td class="textAlignLeft">
+                &nbsp;&nbsp;&nbsp;&nbsp;备注：本文书一式三份，一份主送当地人民政府（矿山安全监管部门），一份报送国家矿山安全监察局，一份存档。
+              </td>
+            </table>
+          </div>
+        </div>
+      </div>
+    </let-main>
+  </div>
+</template>
+
+<script>
+import letMain from "@/views/make-law-writ/components/let-main.vue";
+import GoDB from "@/utils/godb.min.js";
+import { getDangerObject, getDocNumber } from "@/utils/setInitPaperData";
+export default {
+  name: "Let106",
+  props: {
+    corpData: {
+      type: Object,
+      default: () => {},
+    },
+    docData: {
+      type: Object,
+      default: () => {
+        return {
+          docTypeNo: null,
+          docTypeName: null,
+        };
+      },
+    },
+  },
+  components: {
+    letMain,
+  },
+  data() {
+    return {
+      letData: {},
+      options: {
+        cellIdx12: {
+          page: "3",
+          key: "cellIdx12",
+        },
+      },
+      editData: {}, // 回显数据
+    };
+  },
+  created() {
+    this.initData();
+  },
+  watch: {
+    "corpData.corpId"(val) {
+      if (val) {
+        this.initData();
+      }
+    },
+  },
+  methods: {
+    async initData() {
+      const db = new GoDB(this.$store.state.DBName);
+      const corpBase = db.table("corpBase");
+      //查询符合条件的记录
+      const corp = await corpBase.find((item) => {
+        return item.corpId == this.corpData.corpId;
+      });
+      const wkPaper = db.table("wkPaper");
+      const caseId = this.corpData.caseId;
+      const checkPaper = await wkPaper.findAll((item) => {
+        return (
+          item.caseId === caseId &&
+          item.paperType === this.docData.docTypeNo &&
+          item.delFlag !== "1"
+        );
+      });
+      if (checkPaper.length > 0) {
+        // 回显
+        this.letData = JSON.parse(checkPaper[0].paperContent);
+        this.editData = checkPaper[0];
+      } else {
+        // 创建初始版本
+        // 1.生成文书编号
+        let { num0, num1, num3, num4 } = await getDocNumber(
+          db,
+          this.docData.docTypeNo,
+          caseId,
+          this.$store.state.user
+        );
+        this.letData = {
+          cellIdx0: num0, // 文书号
+          cellIdx0TypeTextItem: num0, // 文书号
+          cellIdx1: num1, // 文书号
+          cellIdx1TypeTextItem: num1, // 文书号
+          cellIdx2: num3, // 文书号
+          cellIdx2TypeTextItem: num3, // 文书号
+          cellIdx3: num4, // 文书号
+          cellIdx3TypeTextItem: num4, // 文书号
+          cellIdx4: corp.corpName ? corp.corpName : null, // corpname
+          cellIdx4TypeTextItem: corp.corpName ? corp.corpName : null, // corpname
+          cellIdx5: null, // 签发人
+          cellIdx6: null, // 局
+          cellIdx7: null, // 市（县、区）
+          cellIdx8: null, // 年
+          cellIdx9: null, // 月
+          cellIdx10: null, // 日
+          cellIdx11: null, // 局
+          cellIdx12: null, // 送件人（签名)
+          cellIdx13: null, //日期
+          cellIdx14: null, // 收件人（签名)
+          cellIdx15: null, // 日期
+          cellIdx16: null, // 报送
+          cellIdx17: null, // 
+          cellIdx18: null, // 日期
+          cellIdx19: null, // 附件
+        };
+      }
+      await db.close();
+    },
+    goBack({ page }) {
+      // 返回选择企业
+      this.$emit("go-back", { page });
+    },
+    commandFill(key, title, type) {
+      // 判断是否可编辑
+      if (this.$refs.letMain.canEdit) {
+        // 文书各个字段点击打开左侧弹出编辑窗口
+        let dataKey = `${key}Type${type}`;
+        if (key === "cellIdx12") {
+          this.options[key] = {
+            page: "3",
+            key: key,
+          };
+          dataKey = "dangerItemObject";
+        }
+        this.$refs.letMain.commandFill(
+          key,
+          dataKey,
+          title,
+          type,
+          this.letData[dataKey],
+          this.options[key]
+        );
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/let";
+</style>
